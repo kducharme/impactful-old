97aaca81375d28b73f9151454f8963c96a478b9f
Kyle Ducharme <ducharme.kyle@gmail.com>
2018-06-02 15:13:29 -0500

    react and sass setup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<p>checking if it works!</p>, document.getElementById('root'));
 registerServiceWorker();
